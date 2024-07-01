26b009600cc997ed0f9552fe3caf36d8467f35a5
Diana Sparynyak <dianasparynyak@mbp-diana.home>
2024-07-01 17:00:40 -0400

    finalized bottom sheet scrolling fix

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -32,7 +32,7 @@ import {Navigation} from 'react-native-feather';
 
 const Home = ({navigation}: any) => {
   const [visible, setVisible] = useState(false);
-  const [bottomSheetIndex, setBottomSheetIndex] = useState(1);
+  const [bottomSheetIndex, setBottomSheetIndex] = useState(3);
 
   const cameraRef = useRef<any>(null);
 
