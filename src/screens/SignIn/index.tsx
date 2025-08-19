98c4f52c8d6992c9dc93cd8d5b1302eebef4f2fd
Michael <you@example.com>
2025-08-19 12:00:13 +0300

    fix for keyboard closing buttons on signin, verification pages

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,13 +1,20 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, Text, Dimensions, Image} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import {useTranslation} from 'react-i18next';
 import {Button} from '@styled/buttons';
 import {PhoneInp} from '@styled/inputs';
-
 import {useNavigation} from '@react-navigation/native';
-
 import {useTheme} from '@context/ThemeProvider';
-
 import {dp} from '@utils/dp';
 import useStore from '@state/store';
 import {GeneralAuthNavigationProp} from '@app-types/navigation/AuthNavigation.ts';
@@ -16,18 +23,15 @@ const SignIn = () => {
   const {sendOtp} = useStore.getState();
   const [isLoading, setIsLoading] = useState(false);
   const {t} = useTranslation();
-
+  const scrollViewRef = useRef<ScrollView>(null);
   const {theme} = useTheme();
-
   const navigation = useNavigation<GeneralAuthNavigationProp<'SignIn'>>();
-
   const [phone, setPhone] = useState('');
 
   const validate = (number: string) => {
     if (number.length === 18) {
       return true;
     }
-
     return false;
   };
 
@@ -46,35 +50,55 @@ const SignIn = () => {
   };
 
   return (
-    <View style={{...styles.container, backgroundColor: theme.mainColor}}>
-      <View style={styles.topContainer}>
-        <Image source={require('../../assets/icons/small-icon.png')} />
-      </View>
-      <View style={styles.middleContainer}>
-        <Text style={{...styles.text, color: theme.textColor}}>
-          {t('app.auth.enterPhone')}
-        </Text>
-        <Text style={styles.descriptionText}>
-          {t('app.auth.sendActivationCode')}
-        </Text>
-
-        <PhoneInp phoneNumber={phone} setPhoneNumber={setPhone} />
-        <View style={{paddingTop: dp(100)}}>
-          <Button
-            label={t('common.buttons.next')}
-            color={phone ? 'blue' : 'grey'}
-            disabled={phone ? false : true}
-            onClick={login}
-            showLoading={isLoading}
-          />
-        </View>
-      </View>
-      <View style={styles.bottomContainer} />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <KeyboardAvoidingView
+        style={[
+          styles.keyboardAvoidingView,
+          {backgroundColor: theme.mainColor},
+        ]}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? dp(40) : 0}>
+        <ScrollView
+          ref={scrollViewRef}
+          contentContainerStyle={styles.scrollContainer}
+          keyboardShouldPersistTaps="handled">
+          <View style={styles.container}>
+            <View style={styles.topContainer}>
+              <Image source={require('../../assets/icons/small-icon.png')} />
+            </View>
+            <View style={styles.middleContainer}>
+              <Text style={[styles.text, {color: theme.textColor}]}>
+                {t('app.auth.enterPhone')}
+              </Text>
+              <Text style={styles.descriptionText}>
+                {t('app.auth.sendActivationCode')}
+              </Text>
+              <PhoneInp phoneNumber={phone} setPhoneNumber={setPhone} />
+              <View style={styles.buttonContainer}>
+                <Button
+                  label={t('common.buttons.next')}
+                  color={phone ? 'blue' : 'grey'}
+                  disabled={phone ? false : true}
+                  onClick={login}
+                  showLoading={isLoading}
+                />
+              </View>
+            </View>
+            <View style={styles.bottomContainer} />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
+  keyboardAvoidingView: {
+    flex: 1,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+  },
   container: {
     flex: 1,
     flexDirection: 'column',
@@ -85,13 +109,12 @@ const styles = StyleSheet.create({
     flex: 2,
     justifyContent: 'flex-end',
     alignItems: 'center',
-    marginBottom: dp(20),
   },
   middleContainer: {
     flex: 4,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: dp(30),
+    marginTop: dp(10),
   },
   bottomContainer: {
     flex: 4,
@@ -100,33 +123,15 @@ const styles = StyleSheet.create({
     paddingTop: dp(20),
     width: dp(240),
   },
-  header: {
-    fontSize: dp(20),
-    fontWeight: 'bold',
-    color: '#20315f',
-    paddingTop: dp(30),
-  },
-  phoneInputContainer: {
-    alignSelf: 'center',
-    width: (Dimensions.get('window').width * 2) / 3,
-    paddingTop: dp(50),
-  },
-  loginForm: {
-    display: 'flex',
-    justifyContent: 'center',
+  text: {
+    fontSize: dp(24),
   },
   descriptionText: {
     color: 'rgba(0, 0, 0, 1)',
     fontSize: dp(16),
   },
-  text: {
-    fontSize: dp(24),
-  },
-  buttonActive: {
-    backgroundColor: '#0B68E1',
-  },
-  buttonDisabled: {
-    backgroundColor: '#A3A3A6',
+  buttonContainer: {
+    paddingTop: dp(100),
   },
 });
 
